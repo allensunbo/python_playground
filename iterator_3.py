18f1c0e2b5e3044a341255e0e25d8d4ac1617656
axplay0829@gmail.com <axplay0829@gmail.com>
2014-12-10 10:57:32 -0500

    add another iterator

---
+++ iterator_3.py
@@ -0,0 +1,22 @@
+def readfiles(filenames):
+    for f in filenames:
+        for line in open(f):
+            yield line
+
+
+def grep(pattern, lines):
+    return (line for line in lines if pattern in line)
+
+
+def printlines(lines):
+    for line in lines:
+        print(line)
+
+
+def main(pattern, filenames):
+    lines = readfiles(filenames)
+    lines = grep(pattern, lines)
+    printlines(lines)
+
+if __name__ == '__main__':
+    main('mb', ['c:\passwords.txt'])
